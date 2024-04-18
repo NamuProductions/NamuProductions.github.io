3445f792fc15c1362ab68fef26da21c403d5c212
ryan <tvmuixi@hotmail.com>
2024-04-18 20:00:25 +0100

    corrected a bug and createt apiquiz

--- Functions/1chooseTheme.js
+++ Functions/1chooseTheme.js
@@ -2,7 +2,7 @@ export function chooseTheme() {
     const chooseTheme = document.createElement('div');
     chooseTheme.innerHTML = `
     <div id="chooseTheme" class="container">
-         <h1>¡Choose theme!</h1>-->
+         <h1>¡Choose theme!</h1>
     <div id="sports" class="container1">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe8-rdT96iEIVNIQ2rd0a8Jvt6R3ML9YDK-Q&s" alt="Sports Image">
         <div class="overlay-text">SPORTS</div>
