30520c3ab7d285881de459d5ed0aeaa034fd17be
ryan <tvmuixi@hotmail.com>
2024-04-29 11:33:10 +0100

    update ya no existe

--- Functions/displayQuestions.js
+++ Functions/displayQuestions.js
@@ -3,14 +3,12 @@ import {displayQuestion} from "./displayQuestion.js";
 
 export function displayQuestions(questions) {
 
-
     const apiQuizContainer = document.createElement('div');
     apiQuizContainer.classList.add('container1');
 
     const questionContainer = document.createElement('div');
     questionContainer.classList.add('question-container');
     apiQuizContainer.appendChild(questionContainer);
-
     displayQuestion(questions, questionContainer, apiQuizContainer);
 
     apiQuizContainer.style.display = 'block';
