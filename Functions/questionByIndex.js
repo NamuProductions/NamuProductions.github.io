c2b5c2a0710e26fb95fa6e4cdfffe6e0b1a07e14
ryan <tvmuixi@hotmail.com>
2024-04-30 19:51:08 +0100

    added totalQuestions

--- Functions/questionByIndex.js
+++ Functions/questionByIndex.js
@@ -1,13 +1,15 @@
-export function questionByIndex (questions, gameState) {
-const currentIndex = gameState.currentIndex;
-const currentQuestion = questions[currentIndex];
-const questionText = currentQuestion.question;
-const correctAnswer = currentQuestion.correct_answer;
-const possibleAnswers = [...currentQuestion.incorrect_answers, correctAnswer];
+export function questionByIndex(questions, gameState) {
+    const currentIndex = gameState.currentIndex;
+    const currentQuestion = questions[currentIndex];
+    const questionText = currentQuestion.question;
+    const correctAnswer = currentQuestion.correct_answer;
+    const possibleAnswers = [...currentQuestion.incorrect_answers, correctAnswer];
+    const totalQuestions = questions.lenght;
 
-return {
-    question: questionText,
-    correctAnswer: correctAnswer,
-    possibleAnswers: possibleAnswers
-};
+    return {
+        question: questionText,
+        correctAnswer: correctAnswer,
+        possibleAnswers: possibleAnswers,
+        totalQuestions: totalQuestions
+    };
 }
