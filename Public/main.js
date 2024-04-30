47fe8bfbb13c5f1ae01cd354d235efa218f26364
ryan <tvmuixi@hotmail.com>
2024-04-30 20:19:32 +0100

    creo que se puede simplificar asi

--- Public/main.js
+++ Public/main.js
@@ -1,17 +1,30 @@
-import { displayQuestions } from "../Functions/displayQuestions.js";
-import { fetchQuestions } from "../Functions/fetchQuestions.js";
+import { onStart } from "../Functions/onStart.js";
 
-document.addEventListener("DOMContentLoaded", async function () {
+document.addEventListener("DOMContentLoaded", function () {
     const startButton = document.getElementById('startButton');
-    startButton.addEventListener('click', async function () {
-        try {
-            const root = document.getElementById('root');
-            const questions = await fetchQuestions();
-            const quizContainer = displayQuestions(questions);
-            root.innerHTML = "";
-            root.appendChild(quizContainer);
-        } catch (error) {
-            console.error('Error initializing quiz:', error);
-        }
-    });
+    startButton.addEventListener('click', onStart);
 });
+
+
+
+
+// import { fetchQuestions } from "../Functions/fetchQuestions.js";
+// import {render} from "../Functions/render.js";
+// import {onStart} from "../Functions/onStart.js";
+// import {onAnswer} from "../Functions/onAnswer.js";
+// import {onNext} from "../Functions/onNext.js";
+//
+// document.addEventListener("DOMContentLoaded", async function () {
+//     const startButton = document.getElementById('startButton');
+//     startButton.addEventListener('click', async function () {
+//         try {
+//             const root = document.getElementById('root');
+//             const questions = await fetchQuestions();
+//             const quizContainer = render(gameState, onStart, onAnswer, onNext);
+//             root.innerHTML = "";
+//             root.appendChild(quizContainer);
+//         } catch (error) {
+//             console.error('Error initializing quiz:', error);
+//         }
+//     });
+// });
