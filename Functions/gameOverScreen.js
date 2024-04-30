e56c25f4d51c94b519d2a708957ef1588c286b24
ryan <tvmuixi@hotmail.com>
2024-04-30 19:51:37 +0100

    implemented gameOverScreen

--- Functions/gameOverScreen.js
+++ Functions/gameOverScreen.js
@@ -1,5 +1,19 @@
 export function gameOverScreen(onStart) {
     const screen = document.createElement('div');
+    const apiQuizContainer = document.createElement('.container1');
+
+    screen.classList.add('end-screen');
+
+    const scoreMessage = document.createElement('h2');
+    scoreMessage.textContent = `Your score is: ${score} / ${totalQuestions}`;
+    screen.appendChild(scoreMessage);
+
+    const restartButton = document.createElement('button');
+    restartButton.textContent = 'New Game';
+    restartButton.classList.add('restart-button');
+    screen.appendChild(restartButton);
+
+    apiQuizContainer.appendChild(screen);
 
     return screen;
 }
