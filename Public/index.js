0d6789c7eb333bd9322898092c2220cfbfdd24d5
ryan <tvmuixi@hotmail.com>
2024-04-30 21:01:53 +0100

    simplificando index y exportando sus valores ya que es donde guardaremos los datos

--- Public/index.js
+++ Public/index.js
@@ -1,15 +1,5 @@
-import { render } from "functions/render.js";
-import {onAnswer} from "../Functions/onAnswer.js";
-import {onNext} from "../Functions/onNext.js";
+export { gameState, currentQuestionIndex, score };
 
 let gameState = 'UNSTARTED';
 let currentQuestionIndex = 0;
 let score = 0;
-
-function onStart() {
-    gameState = 'PLAYING';
-    render(gameState, onStart, onAnswer, onNext);
-}
-
-
-
