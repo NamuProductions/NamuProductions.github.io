faf789733cc375a9f4872b92660a7f9413827b6c
ryan <tvmuixi@hotmail.com>
2024-04-30 21:01:07 +0100

    poniendo bien los nombres

--- Functions/render.js
+++ Functions/render.js
@@ -1,3 +1,7 @@
+import {unstartedScreen} from "./unstartedScreen.js";
+import {questionsScreen} from "./questionsScreen.js";
+import {gameOverScreen} from "./gameOverScreen.js";
+
 export function render(gameState, onStart, onAnswer, onNext) {
         const root = document.getElementById('root');
     root.innerHTML = "";
@@ -5,10 +9,8 @@ export function render(gameState, onStart, onAnswer, onNext) {
     if (gameState === 'UNSTARTED') {
         root.append(unstartedScreen(gameState, onStart));
     } else if (gameState === 'PLAYING') {
-        root.append(questionsScreen(state, onAnswer, onNext))
+        root.append(questionsScreen(gameState, onAnswer, onNext))
     } else if (gameState === 'GAME_OVER') {
-        root.append(gameOverScreen(state, onStart));
+        root.append(gameOverScreen(gameState, onStart));
     }
-
-
 }
