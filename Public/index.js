3431558b5681a66d9cc5f10fc400f83438c4b2dd
ryan <tvmuixi@hotmail.com>
2024-05-01 12:14:27 +0100

    questions

--- Public/index.js
+++ Public/index.js
@@ -2,10 +2,10 @@ import { render } from "../Functions/render.js";
 import { onAnswer } from "../Functions/onAnswer.js";
 import { onNext } from "../Functions/onNext.js";
 
-export function onStart(gameState) {
+export function onStart(gameState, questions) {
     gameState.gameState = 'PLAYING';
     console.log(gameState);
-    render(gameState, onStart, onAnswer, onNext);
+    render(gameState, onStart, onAnswer, onNext, questions);
     return {gameState};
 }
 
