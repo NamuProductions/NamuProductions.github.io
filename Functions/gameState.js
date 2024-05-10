d0f863949a35228c014b3ca2015ec2544397be48
fizdalf <fizdalf@gmail.com>
2024-05-10 16:03:21 +0100

    Refactor rendering functions to accept gameState
    The rendering functions `renderStartScreen`, `renderGameScreen`, and `renderGameOverScreen` have been refactored to accept gameState as a reference, instead of retrieving this value separately. This change increases code efficiency and clarity by reducing redundancy, and places the responsibility of maintaining game state with the gameState object. Some stylistic adjustments have also been applied for better readability.

--- Functions/gameState.js
+++ Functions/gameState.js
@@ -10,9 +10,10 @@ export function setQuestions(questions) {
     gameState.questions = questions;
     gameState.totalQuestions = questions.length;
 }
+
 export function startGame() {
     gameState.gameStage = 'PLAYING';
-    }
+}
 
 export function nextQuestion(gameState) {
     gameState.currentQuestionIndex++;
@@ -23,7 +24,7 @@ export function nextQuestion(gameState) {
     }
 }
 
-export function restartGame () {
+export function restartGame() {
     gameState.gameStage = 'UNSTARTED';
     gameState.currentQuestionIndex = 0;
     gameState.score = 0;
