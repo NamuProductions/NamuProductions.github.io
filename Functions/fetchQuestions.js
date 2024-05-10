4b08d27c674381712e980cf615e650d5b867a8f2
ryan <tvmuixi@hotmail.com>
2024-05-10 12:56:14 +0100

    solucionado el problema de los caracteres especiales

--- Functions/fetchQuestions.js
+++ Functions/fetchQuestions.js
@@ -7,9 +7,29 @@ export async function fetchQuestions() {
         }
         const data = await response.json();
 
-        return data.results;
+        return decodeHTMLCharacters(data.results);
     } catch (error) {
         console.error('Error fetching questions:', error);
         return [];
     }
+}
+
+function decodeHTMLCharacters(results) {
+    return results.map(question => {
+        const decodedQuestion = decodeHTML(question.question);
+        const decodedIncorrectAnswers = question.incorrect_answers.map(answer => decodeHTML(answer));
+        const decodedCorrectAnswer = decodeHTML(question.correct_answer);
+        return {
+            ...question,
+            question: decodedQuestion,
+            incorrect_answers: decodedIncorrectAnswers,
+            correct_answer: decodedCorrectAnswer
+        };
+    });
+}
+
+function decodeHTML(html) {
+    const txt = document.createElement('textarea');
+    txt.innerHTML = html;
+    return txt.value;
 }
