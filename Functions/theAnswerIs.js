902c7827d750645a033a7c5a4ab38a112a25574c
ryan <tvmuixi@hotmail.com>
2024-05-19 22:44:28 +0100

    según la dificultad se suman 1, 2 o 3 puntos por pregunta implementado y funciona bien

--- Functions/theAnswerIs.js
+++ Functions/theAnswerIs.js
@@ -1,26 +1,47 @@
 export function theAnswerIs(optionButton, currentQuestion, gameState) {
     const selectedOption = optionButton.textContent;
     const correctAnswer = currentQuestion.correctAnswer;
-    const isCorrect = selectedOption === correctAnswer
+    const isCorrect = selectedOption === correctAnswer;
 
     gameState.lastSelectedOption = selectedOption;
 
     if (isCorrect) {
         console.log('Correct ' + correctAnswer);
-        gameState.score = updateScore(gameState, true)
+        gameState.score = updateScore(gameState, isCorrect);
     } else {
         console.log(selectedOption + ' is wrong, the correct answer is: ' + correctAnswer);
     }
+
     const optionButtons = document.querySelectorAll('.option-button');
     optionButtons.forEach(button => {
         button.disabled = true;
     });
-    return {
+
+   return {
         isCorrect: isCorrect,
         correctAnswer: correctAnswer
     };
 }
 
 function updateScore(gameState, isCorrect) {
-    return isCorrect ? gameState.score + 1 : gameState.score;
+    let scoreIncrease = 0;
+
+    if (isCorrect) {
+        switch (gameState.selectedDifficulty.toLowerCase()) {
+            case 'easy':
+                scoreIncrease = 1;
+                break;
+            case 'medium':
+                scoreIncrease = 2;
+                break;
+            case 'hard':
+                scoreIncrease = 3;
+                break;
+            default:
+                console.error('Unknown difficulty:', gameState.selectedDifficulty);
+                break;
+        }
+    }
+
+    return gameState.score + scoreIncrease;
 }
